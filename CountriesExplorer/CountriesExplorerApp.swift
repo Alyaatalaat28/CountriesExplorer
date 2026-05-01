daf04f499b23e54c49374f1d15ab9cbcdc277d56
Alyaatalaat28 <alyaatalaat205@gmail.com>
2026-05-01 13:22:55 +0300

    Initial Commit

---
+++ CountriesExplorer/CountriesExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesExplorerApp.swift
+//  CountriesExplorer
+//
+//  Created by Alyaa on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct CountriesExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
